47b5a967bb9b248e41158dc1fe10c55ba0f4f645
nirvanasupermind <nirvana.supermind@gmail.com>
2025-02-28 22:51:13 -0800

    added tests

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,8 @@
     
 int main(){
 tachyon_stl_setup();
-(*tachyon_internal::decode_func(println))({(*tachyon_internal::decode_func(tachyon_internal::get_member(tachyon_internal::decode_obj(Math),"atan2")))({Math,2.00000000000000000,3.00000000000000000})});
+(*tachyon_internal::decode_func(println))({1.28247467873076837});
+(*tachyon_internal::decode_func(println))({0.588002603547567504});
 
 tachyon_internal::free_all();
 return 0;
